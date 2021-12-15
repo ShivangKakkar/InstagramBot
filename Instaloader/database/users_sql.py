53a6c727aff622f3913d6d3c375c0ebbdb7b28e6
Stark Bots <88478059+StarkBotsIndustries@users.noreply.github.com>
2021-12-15 09:31:20 +0530

    add columns in db for credentials

--- Instaloader/database/users_sql.py
+++ Instaloader/database/users_sql.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String
 from Instaloader.database import BASE, SESSION
 
 
@@ -6,9 +6,13 @@ class Users(BASE):
     __tablename__ = "users"
     __table_args__ = {'extend_existing': True}
     user_id = Column(Integer, primary_key=True)
+    insta_username = Column(String)
+    insta_password = Column(String)
 
-    def __init__(self, user_id):
+    def __init__(self, user_id, insta_username=None, insta_password=None):
         self.user_id = user_id
+        self.insta_username = insta_username
+        self.insta_password = insta_password
 
 
 Users.__table__.create(checkfirst=True)
@@ -19,3 +23,25 @@ async def num_users():
         return SESSION.query(Users).count()
     finally:
         SESSION.close()
+
+
+async def set_info(user_id, username, password):
+    q = SESSION.query(Users).get(user_id)
+    if q:
+        q.insta_username = username
+        q.insta_password = password
+    else:
+        SESSION.add(Users(user_id, username, password))
+    SESSION.commit()
+
+
+async def get_info(user_id):
+    q: Users = SESSION.query(Users).get(user_id)
+    if q and q.insta_password:
+        info = q.insta_username, q.insta_password
+        SESSION.close()
+        return info
+    else:
+        SESSION.add(Users(user_id))
+        SESSION.commit()
+        return None, None
